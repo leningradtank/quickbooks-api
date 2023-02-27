84869ce49b7ddff6f33e6b29d53a12191a237265
ridersamz2000 <47441021+ridersamz2000@users.noreply.github.com>
2023-02-27 22:46:07 +0300

    added list append

--- qb_multiple_je.py
+++ qb_multiple_je.py
@@ -29,16 +29,15 @@ df_upload = pd.read_csv('qb_data_2.csv')
 account_ref= Ref()
 account_ref.value = 114
 
+#decalre a list of journal entries
 
+journal_entry = JournalEntry()
+journal_entry.Line = []
 # validation for groupby and sum for account balance, calculate diff and should be zero.
 # sort values +ve and -ve, then iterate through debits, credits
 
 
-for entry in range(0,len(df)):
-
-    
-
-for index, row in df_upload.iterrows():
+for entry in range(0,len(df_upload)):
     account_ref.name = df_upload['Account'].iloc[entry]
     account_ref.type = df_upload['Type'].iloc[entry]
 
@@ -47,41 +46,59 @@ for index, row in df_upload.iterrows():
     detail_one.AccountRef = account_ref
 
     line_one = JournalEntryLine()
-    journal_entry = JournalEntry()
-
+    
     line_one.JournalEntryLineDetail = detail_one
     line_one.LineNum = 0
     line_one.Description = df_upload['Description'].iloc[entry]
 
-    amount = df_upload['Balance'].iloc[index]
+    amount = df_upload['Balance'].iloc[entry]
 
     line_one.Amount = amount.astype(str)
 
     line_one.DetailType = "JournalEntryLineDetail"
 
-    #detail two 
-    detail_two = JournalEntryLineDetail()
+    journal_entry.Line.append(line_one)
 
-    detail_two.PostingType = df_upload['PostingType'].iloc[index+1] #journal_input["Line"][0]['JournalEntryLine1']['JournalEntryLineDetail']['PostingType']
-    detail_two.AccountRef = account_ref
+journal_entry.save(qb=client)
 
-    line_two = JournalEntryLine()
+    
 
-    line_two.JournalEntryLineDetail = detail_two
-    line_two.LineNum = 1
-    line_two.Description = df_upload['Description'].iloc[index+1]
+    # journal_entry = JournalEntry()
 
-    amount = df_upload['Balance'].iloc[index+1]
+    # journal_entry.Line = [line_one, line_two]
+    # #append lines in the for loops in the list 
 
-    line_two.Amount = amount.astype(str)
+    # journal_entry.save(qb=client)
 
-    line_two.DetailType = "JournalEntryLineDetail"
 
-    journal_entry = JournalEntry()
-    journal_entry.Line = [line_one, line_two]
-    #append lines in the for loops in the list 
+# for index, row in df_upload.iterrows():
 
-    journal_entry.save(qb=client)
+
+
+
+#     #detail two 
+#     detail_two = JournalEntryLineDetail()
+
+#     detail_two.PostingType = df_upload['PostingType'].iloc[index+1] #journal_input["Line"][0]['JournalEntryLine1']['JournalEntryLineDetail']['PostingType']
+#     detail_two.AccountRef = account_ref
+
+#     line_two = JournalEntryLine()
+
+#     line_two.JournalEntryLineDetail = detail_two
+#     line_two.LineNum = 1
+#     line_two.Description = df_upload['Description'].iloc[index+1]
+
+#     amount = df_upload['Balance'].iloc[index+1]
+
+#     line_two.Amount = amount.astype(str)
+
+#     line_two.DetailType = "JournalEntryLineDetail"
+
+#     journal_entry = JournalEntry()
+#     journal_entry.Line = [line_one, line_two]
+#     #append lines in the for loops in the list 
+
+#     journal_entry.save(qb=client)
 
 
 
