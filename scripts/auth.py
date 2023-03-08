ebfef7a23d2d547f4e0c48489c531a6d06ae6af4
ridersamz2000 <47441021+ridersamz2000@users.noreply.github.com>
2023-03-08 14:41:16 +0100

    update

---
+++ scripts/auth.py
@@ -0,0 +1,24 @@
+from intuitlib.client import AuthClient
+from intuitlib.enums import Scopes
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+client_id= os.getenv('client_id'),
+client_secret= os.getenv('client_secret')
+environment='sandbox'
+redirect_uri='http://localhost:8000/callback'
+company_id=os.getenv('company_id')
+
+scopes = [
+    Scopes.ACCOUNTING,
+]
+
+auth_client = AuthClient(client_id, client_secret, redirect_uri, environment )
+url = auth_client.get_authorization_url(scopes)
+
+print(url)
+
+# token = auth_client.get_bearer_token(auth_code, realm_id=realm_id)
+
